6bced611cc65e2c0f357e84967d58f02496ec1ed
Randula <randularavindu97@gmail.com>
2023-08-05 12:49:51 +0530

    main screen

--- AutomaticFishFeederApp/app/src/main/java/com/company/automaticfishfeederapp/MainActivity.java
+++ AutomaticFishFeederApp/app/src/main/java/com/company/automaticfishfeederapp/MainActivity.java
@@ -2,13 +2,36 @@ package com.company.automaticfishfeederapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_login,btn_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_login = (Button) findViewById(R.id.btn_login);
+        btn_register = (Button) findViewById(R.id.btn_register);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,CreateAccountActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
