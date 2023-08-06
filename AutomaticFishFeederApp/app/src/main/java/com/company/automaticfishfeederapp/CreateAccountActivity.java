18a0b2f6532405f4494d1f6b8436880cd8dd2c08
Randula <randularavindu97@gmail.com>
2023-08-06 14:59:34 +0530

    login register

--- AutomaticFishFeederApp/app/src/main/java/com/company/automaticfishfeederapp/CreateAccountActivity.java
+++ AutomaticFishFeederApp/app/src/main/java/com/company/automaticfishfeederapp/CreateAccountActivity.java
@@ -2,13 +2,26 @@ package com.company.automaticfishfeederapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
+    private Button btn_createAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        btn_createAccount = (Button) findViewById(R.id.buttonCreateAccount);
+        btn_createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CreateAccountActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- AutomaticFishFeederApp/app/src/main/java/com/company/automaticfishfeederapp/LoginActivity.java
+++ AutomaticFishFeederApp/app/src/main/java/com/company/automaticfishfeederapp/LoginActivity.java
@@ -2,13 +2,26 @@ package com.company.automaticfishfeederapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button btn_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_login = (Button) findViewById(R.id.buttonLogin);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+                //startActivity(intent);
+            }
+        });
     }
 }
